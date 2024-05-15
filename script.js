d39000ef8a18cc1b59a8f441c808a29f6ba3975d
saffer001 <21124395+saffer001@users.noreply.github.com>
2024-05-15 23:44:41 +0200

    Finish functionality, start on designing.

--- script.js
+++ script.js
@@ -34,20 +34,56 @@ const Board = (function() {
             let arrayResetValue = "□";
             grid.fill(arrayResetValue);
             Board.drawGrid()
-
+            GameControls.updateCurrentPlayerDisplay()
         },
         playerTurn
     };
 })();
-// Factory function to create the player objects.
-function createPlayer(playerName, playerMark) {
-    let playerScore = 0;
+
+const GameControls = (function() {
+    const playerOneScoreDisplay = document.getElementById("p-player1Score");
+    const playerTwoScoreDisplay = document.getElementById("p-player2Score");
+    const currentPlayerDisplay = document.getElementById("p-currentPlayer");
+    const resetButton = document.getElementById("button-resetButton")
     return {
-        playerName,
-        incrementScore: () => playerScore++,
-        displayScore: () => playerScore,
-    };
-};
+        updateScoresDisplay: () => {
+            playerOneScoreDisplay.textContent = `❌ score: ${players.displayPlayerOneScore()}`
+            playerTwoScoreDisplay.textContent = `⭕ score: ${players.displayPlayerTwoScore()}`
+        },
+        updateCurrentPlayerDisplay: (nextMove) => {
+            if (Board.playerTurn % 2 == 0) {
+                currentPlayerDisplay.textContent = `Next move is for ❌`
+            } else {
+                currentPlayerDisplay.textContent = `Next move is for ⭕`
+            }
+        },
+        resetGame: () => {
+            resetButton.addEventListener("click", () => {
+                Board.resetBoard()
+                players.resetScores()
+                GameControls.updateScoresDisplay()
+            })
+        }
+    }
+       
+})();
+//IIFE to create the players and track scores.
+const players = (function() {
+    let playerOneScore = 0
+    let playerTwoScore = 0
+    return {
+        incrementPlayerOneScore: () => playerOneScore++,
+        incrementPlayerTwoScore: () => playerTwoScore++,
+        displayPlayerOneScore: () => playerOneScore,
+        displayPlayerTwoScore: () => playerTwoScore,
+        resetScores: () => {
+            playerOneScore = 0;
+            playerTwoScore = 0;
+        }
+
+    }
+})();
+
 //IIFE to handle game flow
 const gameMaster = (function() {
     return {
@@ -63,25 +99,32 @@ const gameMaster = (function() {
                 (Board.returnGrid(0) == playerMark && Board.returnGrid(4) == playerMark && Board.returnGrid(8) == playerMark) ||
                 (Board.returnGrid(6) == playerMark && Board.returnGrid(4) == playerMark && Board.returnGrid(2) == playerMark)) {
                     console.log(`gameover, playerwin, winner is ${playerMark}`); // for testing only
-                    incrementScore(playerMark)
+                    if (playerMark == "❌") {
+                        players.incrementPlayerOneScore()
+                        alert("❌ scored a point!")
+                        GameControls.updateScoresDisplay()
+                    } else {
+                        players.incrementPlayerTwoScore()
+                        alert("⭕ scored a point!")
+                        GameControls.updateScoresDisplay()
+                    }
                     Board.resetBoard();
             } else if (allCellsAreFilled) {
-                    console.log("gameover tie");
+                    alert("It's a tie!")
+                    //console.log("gameover tie");
                     Board.resetBoard()
                     return allCellsAreFilled;
             }
         },
-        //Checks for a tie.
-        checkForTie: () => {
-            const allCellsAreFilled = Board.returnGridCopy().every(item => item !="□");
-            return allCellsAreFilled;
-        },
+        // Adds a mark of a player to a certain index in the grid.
         addMark: (index, playerMark) => {
             if (Board.returnGridCopy()[index] == "□") {
                 Board.drawMark(index, playerMark);
-                console.log(`marking ${index} with ${playerMark}`);
+                //console.log(`marking ${index} with ${playerMark}`);
+                Board.playerTurn++
+                //console.log(`Current player turn: ${Board.playerTurn}`);
             } else {
-                console.log("Grid already occupied!");
+                //console.log("Grid already occupied!");
             }
             
         }
@@ -92,22 +135,20 @@ const domInteractions = (function() {
     return {
         cellClicked: (element) => {
             if (Board.playerTurn % 2 == 0) {
-                Board.playerTurn ++
-                console.log("player1 clicked");
+                //console.log("player1 clicked");
                 gameMaster.addMark(element.id,"❌")
                 element.textContent = Board.returnGrid(element.id)
-                console.log(Board.returnGridCopy());
+                //console.log(Board.returnGridCopy());
                 gameMaster.checkForPlayerWin("❌")
-                gameMaster.checkForTie()
+                GameControls.updateCurrentPlayerDisplay()
                 Board.drawGridConsole()
             } else {
-                Board.playerTurn++
-                console.log("player2 clicked");
+                //console.log("player2 clicked");
                 gameMaster.addMark(element.id,"⭕")
-                console.log(Board.returnGridCopy());
+                //console.log(Board.returnGridCopy());
                 element.textContent = Board.returnGrid(element.id)
                 gameMaster.checkForPlayerWin("⭕")
-                gameMaster.checkForTie()
+                GameControls.updateCurrentPlayerDisplay()
                 Board.drawGridConsole()
             }
         }
@@ -115,10 +156,7 @@ const domInteractions = (function() {
 
 })();
 
-
-const playerOne = createPlayer("Jóska");
-const playerTwo = createPlayer("Pista");
-
-
-console.log(`Player one score is: ${playerOne.displayScore()}, player two score is: ${playerTwo.displayScore()}`);
 Board.drawGrid()
+GameControls.updateScoresDisplay()
+GameControls.updateCurrentPlayerDisplay()
+GameControls.resetGame()
